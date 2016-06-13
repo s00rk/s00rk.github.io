5eeed33a49a6ea9670ef019d605f37f7cf30a7a9
Victor Rivera <victor.riverac92@gmail.com>
2016-06-13 13:08:07 -0600

    Traslado web

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var callback = function(){
+	$('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+		if(newWidth < 180)
+		{
+			newWidth = '50%';
+		}
+	    $(this).animate({
+	        width: newWidth,
+	    }, 1000);
+	});
+	$('.icons-red').each(function(){
+		height = $(this).height();
+	    $(this).animate({
+	        height: 14,
+	    }, 2000);
+	});
+	$("html").niceScroll();
+};
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
